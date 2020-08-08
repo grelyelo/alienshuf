9dbde223633e9d8b9e5911ba0bd034eea7f6025a
Timothy L <tlesgaux@protonmail.ch>
2020-08-08 13:32:56 -0500

    added option to specify options, useful in future

--- alienshuf.py
+++ alienshuf.py
@@ -4,6 +4,7 @@ import requests
 from random import sample
 import argparse
 import re
+from copy import deepcopy
 
 #Setup Argparser
 
@@ -14,7 +15,10 @@ parser.add_argument("--count","-c", dest='count', default=1, help="Number of fil
 parser.add_argument("--limit", "-l", dest='limit', default=25, help="Number of links to request from reddit (must be >= count), default 25", type=int)
 parser.add_argument("--sort", "-s", dest='sort', default="hot", help="How to sort subreddit when shuffling post")
 parser.add_argument("--debug", "-d", dest='debug', action='store_true', help="Enables debug mode and prints all debug messages")
+
+
 VALID_SORTS = [ "hot", "top", "new" ]
+DEFAULT_OPTIONS = { "top": { "t": "all" }, "hot": {}, "new": {} }
 VALID_FILETYPES = [None, "jpg", "jpeg", "gif", "webm", "png"]
 
 #If you want, you can change your user agent
@@ -47,14 +51,33 @@ def fixFiletype(filetype):
     else:
         return None
 
-def getPostUrls(subreddit,limit,sort,filetype=None, debug=None):
+def getParamsForSort( sort, limit=None, options=None):   
+    
+    if limit:
+        params = { "limit": str(limit) }
+    else:
+        params = {}
+
+    if options:
+        params.update(options)
+    else:
+        try:
+            params.update(DEFAULT_OPTIONS[sort])
+        except KeyError:
+            raise ValueError("Bad sort '%s'  provided to getParamsForSort" % sort)
+
+    return params
+    
+def getPostUrls(subreddit, limit, sort, filetype=None, options=None, debug=None):
     # Returns count urls from subreddit as a list
     if( not subreddit.isalnum() ):
         return None
     
+    params = getParamsForSort(sort, limit, options)
+
     raw_response = requests.get(
             "https://www.reddit.com/r/" + subreddit + "/" + sort + "/.json",
-            params={"limit": str(limit)},
+            params=params,
             headers={"User-agent": USER_AGENT}
     )
     if debug:
@@ -62,14 +85,16 @@ def getPostUrls(subreddit,limit,sort,filetype=None, debug=None):
     json_response= raw_response.json()
 
     url_list = [post['data']['url'] for post in json_response['data']['children'] if not post['data']['is_self'] ]
+    if debug:
+        print("Got %d urls" % len(url_list))
     if filetype is not None:   
         regex = re.compile(r"\.%s$" % filetype)
         return list(filter(regex.search, url_list))
     else:
         return url_list
 
-def printRandomUrls(subreddit, count, limit, sort, filetype=None, debug=False):
-    url_list = getPostUrls(subreddit, limit, sort, filetype=filetype, debug=debug)
+def printRandomUrls(subreddit, count, limit, sort, filetype=None, options=None, debug=False):
+    url_list = getPostUrls(subreddit, limit, sort, filetype=filetype, options=options, debug=debug)
 
     if len(url_list) > 0:
         for s in sample(url_list, count):
@@ -82,7 +107,9 @@ ARGS.filetype = fixFiletype(ARGS.filetype) # fix the filetype
 
 def main():
     if(validInput(ARGS)):
-        printRandomUrls(ARGS.subreddit, ARGS.count, ARGS.limit, ARGS.sort, ARGS.filetype, ARGS.debug)
+        if ARGS.debug:
+            print(ARGS)
+        printRandomUrls(ARGS.subreddit, ARGS.count, ARGS.limit, ARGS.sort, filetype=ARGS.filetype, options=None, debug=ARGS.debug)
     else:
         print(ERROR_BAD_ARGUMENT)
 
